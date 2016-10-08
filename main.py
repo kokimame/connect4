21df3d0a636336c6c6e56c1f33d668a7ed77dc9a
kokimame <kokimametani@gmail.com>
2016-10-09 02:45:17 +0900

    Display the root window

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import (QApplication, QGraphicsScene, QGraphicsView,
+        QMainWindow, QGraphicsRectItem)
+from PyQt5.QtCore import (Qt)
+from PyQt5.QtGui import (QColor)
+
+class GraphicsView(QGraphicsView):
+    def __init__(self, parent=None):
+        QGraphicsView.__init__(self, parent=parent)
+
+        scene = QGraphicsScene(self)
+
+        item = QGraphicsRectItem(-200, -200, 400, 400)
+        scene.addItem(item)
+        
+        self.setScene(scene)
+
+        self.setBackgroundBrush(QColor(125, 255, 125))
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent=parent)
+        self.setGeometry(100, 100, 600, 600)
+        self.setWindowTitle("Connect 4")
+
+        view = GraphicsView()
+        self.setCentralWidget(view)
 
 
 def main():
-    pass
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec_())
 
 if __name__ == '__main__':
     main()
